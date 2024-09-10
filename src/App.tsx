1ff1333c8aaebe6b108ae51bcf8999849f3ca669
sankhadeepchatterjeee <sankhadeep2007@gmail.com>
2024-09-10 19:22:28 +0530

    add MUI initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+
+import { FC } from "react";
+import { HomePage } from "./Pages/HomePage";
+
+const App: FC = () =>{
+  return (
+    <>
+      <div>
+        <HomePage />
+      </div>
+    
+    </>
+  );
+};
+
+export default App
---
+++ src/Pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+import Header from '../components/Header';
+
+export const HomePage: FC = () => {
+    return (
+        <>
+           <Header />
+        </>
+    );
+};
+
